a2ce1e0dc3b919082910e0dfc7f327bea0d6f78c
School ERP <jasim8799@github.com>
2026-01-15 02:47:15 +0530

    Complete Super Admin dashboard implementation and deployment fixes

--- src/routes/feeStructure.routes.js
+++ src/routes/feeStructure.routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { createFeeStructure, getFeeStructures } = require('../controllers/feeStructure.controller');
 const { authenticate } = require('../middlewares/auth.middleware');
 const { requireRole } = require('../middlewares/role.middleware');
+const { USER_ROLES } = require('../config/constants');
 const { validateSchool } = require('../middlewares/school.middleware');
 
 const router = express.Router();
@@ -11,7 +12,7 @@ router.use(authenticate);
 router.use(validateSchool);
 
 // Only Principal and Operator can access
-router.use(requireRole('PRINCIPAL', 'OPERATOR'));
+router.use(requireRole(USER_ROLES.PRINCIPAL, USER_ROLES.OPERATOR));
 
 router.post('/', createFeeStructure);
 router.get('/', getFeeStructures);
