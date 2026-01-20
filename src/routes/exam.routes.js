871cfdbd5eebae23fd99c3372935376c57963a8a
School ERP <jasim8799@github.com>
2026-01-20 20:54:07 +0530

    demo school105

--- src/routes/exam.routes.js
+++ src/routes/exam.routes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const { createExam, getExamsByClass, updateExam, publishExam } = require('../controllers/exam.controller.js');
+const { authenticate } = require('../middlewares/auth.middleware.js');
+const { attachActiveSession } = require('../middlewares/session.middleware.js');
 const { requireRole } = require('../middlewares/role.middleware.js');
 const { enforceSchoolIsolation } = require('../middlewares/school.middleware.js');
 const { USER_ROLES } = require('../config/constants.js');
@@ -8,6 +10,8 @@ const router = express.Router();
 
 router.post(
   '/',
+  authenticate,
+  attachActiveSession,
   enforceSchoolIsolation,
   requireRole(USER_ROLES.PRINCIPAL, USER_ROLES.OPERATOR),
   createExam
@@ -15,6 +19,8 @@ router.post(
 
 router.get(
   '/',
+  authenticate,
+  attachActiveSession,
   enforceSchoolIsolation,
   requireRole(
     USER_ROLES.PRINCIPAL,
@@ -26,6 +32,8 @@ router.get(
 
 router.put(
   '/:examId',
+  authenticate,
+  attachActiveSession,
   enforceSchoolIsolation,
   requireRole(USER_ROLES.PRINCIPAL, USER_ROLES.OPERATOR),
   updateExam
@@ -33,6 +41,8 @@ router.put(
 
 router.patch(
   '/:examId/publish',
+  authenticate,
+  attachActiveSession,
   enforceSchoolIsolation,
   requireRole(USER_ROLES.PRINCIPAL, USER_ROLES.OPERATOR),
   publishExam
