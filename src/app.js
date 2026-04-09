dd5f63231a21b6a85fbf87aec90b748ed78f1fdc
School ERP <jasim8799@github.com>
2026-04-09 16:53:27 +0530

    174

--- src/app.js
+++ src/app.js
@@ -74,8 +74,8 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '25mb' }));
+app.use(express.urlencoded({ extended: true, limit: '25mb' }));
 
 // Health check route
 app.get('/health', (req, res) => {
