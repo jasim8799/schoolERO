0798296b51c16c9c1e5ef9cdcc929098492696a9
School ERP <jasim8799@github.com>
2026-04-11 23:03:57 +0530

    193

--- src/controllers/hostel.controller.js
+++ src/controllers/hostel.controller.js
@@ -1,4 +1,5 @@
 const Hostel = require('../models/Hostel.js');
+const Room = require('../models/Room.js');
 
 const createHostel = async (req, res) => {
   try {
@@ -29,8 +30,34 @@ const createHostel = async (req, res) => {
 const getHostels = async (req, res) => {
   try {
     const { schoolId } = req.user;
-    const hostels = await Hostel.find({ schoolId });
-    res.json({ success: true, data: hostels });
+
+    const hostels = await Hostel.find({ schoolId }).lean();
+    const hostelIds = hostels.map((h) => h._id);
+
+    const rooms = hostelIds.length > 0
+      ? await Room.find({ hostelId: { $in: hostelIds }, schoolId })
+        .select('hostelId roomNumber totalBeds availableBeds')
+        .lean()
+      : [];
+
+    const roomsByHostel = {};
+    for (const room of rooms) {
+      const key = room.hostelId.toString();
+      if (!roomsByHostel[key]) roomsByHostel[key] = [];
+      roomsByHostel[key].push(room);
+    }
+
+    const enriched = hostels.map((h) => {
+      const key = h._id.toString();
+      const hostelRooms = roomsByHostel[key] || [];
+      return {
+        ...h,
+        rooms: hostelRooms,
+        roomCount: hostelRooms.length,
+      };
+    });
+
+    res.json({ success: true, data: enriched });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
